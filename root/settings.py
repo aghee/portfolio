3566a78496254254772b27caa6be9b7acc2a299a
aghee <agymunzala@gmail.com>
2024-05-31 22:04:44 +0300

    fix:add static_root

--- root/settings.py
+++ root/settings.py
@@ -142,6 +142,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
