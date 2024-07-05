f7b2a7a03b7b9f68e641c068c033df65e2a9e890
aghee <agymunzala@gmail.com>
2024-07-05 17:53:40 +0300

    new update to config

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,2 @@
-if ! command -v pip &> /dev/null
-then
-    echo "pip not found, installing..."
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py --user
-    export PATH=$PATH:~/.local/bin
-fi
-
-# Install Python if not already installed
-if ! command -v python &> /dev/null
-then
-    echo "python not found, installing..."
-    sudo apt-get update
-    sudo apt-get install -y python
-fi
-
 pip install -r requirements.txt
-python manage.py collectstatic
+python3.9 manage.py collectstatic
