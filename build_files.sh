a0bc0fa5477ab3bffd8be45ee3039e5ba796641f
aghee <agymunzala@gmail.com>
2024-07-05 17:41:51 +0300

    update config settings

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,18 @@
-pip3 install -r requirements.txt
-python manage.py collectstatic
+if ! command -v pip &> /dev/null
+then
+    echo "pip not found, installing..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py --user
+    export PATH=$PATH:~/.local/bin
+fi
+
+# Install Python 3.11 if not already installed
+if ! command -v python3.11 &> /dev/null
+then
+    echo "python3.11 not found, installing..."
+    sudo apt-get update
+    sudo apt-get install -y python3.11
+fi
+
+pip install -r requirements.txt
+python3.11 manage.py collectstatic
